00c7be2a61cc35bac9c3884d8f5e4b74c1ec4340
CS-SF-Kon <vladislav.konovalov@evraz.com>
2024-09-24 15:55:40 +0700

    Добавьте файлы проекта.

---
+++ Module5-6/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Module5_6
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
